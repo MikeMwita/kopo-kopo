18bca5c58d4e394b28e19b6c08df0b4583379927
MikeMwita <mwitamike8@gmail.com>
2024-02-03 14:32:14 +0300

    Update Server code

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,9 @@
-package main
+package cmd
 
-import (
-	"github.com/ffc1e12/kopo-kopo-transactionmanagementbackend/internal/router"
-	"log"
-	"net/http"
-)
-
-func main() {
-	r := router.NewRouter()
-
-	port := 8080
-	log.Printf("Server running on port %d...\n", port)
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
+//func main() {
+//	r := router.NewRouter()
+//
+//	port := 8080
+//	log.Printf("Server running on port %d...\n", port)
+//	log.Fatal(http.ListenAndServe(":8080", r))
+//}
